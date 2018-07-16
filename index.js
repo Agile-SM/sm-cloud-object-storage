150dbe6336972a6813457bd0254425e4f83dd648
Oihane <seveoih@gmail.com>
2018-07-16 13:49:04 +0200

    New version -> streaming not call cos everytime

--- index.js
+++ index.js
@@ -52,71 +52,80 @@ class CloudObjectStorage {
     }
 
     streamingFile(item, bucket, req, res, next) {
-        this.cos.getObject({Bucket: bucket, Key: item}, function (error, data) {
-            if (error !== null) {
-                res.status(404).send(error);
+        var COSITEM = this.cos;
+        let file = '.tmp/' + item;
+        fs.readFile(file, function (error, result) {
+            if (result) {
+                doStreaming(item, result, res, req);
             } else {
-                let stream = new Duplex();
-                stream.push(data.Body);
-                stream.push(null);
-                let buf = [];
-                let nb = 0;
-                let file = '.tmp/' + item;
-                stream.on('data', function(chunk) {
-                    buf.push(chunk);
-                    nb += chunk.length;
-                }).on('end', function() {
-                    let buffer = Buffer.concat(buf, nb);
-                    fs.writeFile(file, buffer, "binary", function(err, result) {
-                        if (err) {
-                            return err;
-                        } else {
-                            fs.stat(file, function(error, stats) {
-                                if (error) {
-                                    //	1.	Check if the file exists
-                                    if (error.code === 'ENOENT') {
-                                        // 	->	404 Error if file not found
-                                        return res.sendStatus(404);
-                                    }
-                                    //	2.	IN any other case, just output the full error
-                                    return next(err)
-                                }
-                                let range = req.headers.range;
-                                if (!range) {
-                                    let err = new Error('Wrong range');
-                                    err.status = 416;
-                                    //	->	Send the error and stop the request.
-                                    //return next(err);
-                                }
-                                let positions = range.replace(/bytes=/, '').split('-');
-                                let start = parseInt(positions[0], 10);
-                                let file_size = stats.size;
-                                let end = positions[1] ? parseInt(positions[1], 10) : file_size - 1;
-                                let chunksize = (end - start) + 1;
-                                let head = {
-                                    'Content-Range': 'bytes ' + start + '-' + end + '/' + file_size,
-                                    'Accept-Ranges': 'bytes',
-                                    'Content-Length': chunksize,
-                                    'Content-Type': 'video/mp4'
-                                }
-                                res.writeHead(206, head);
-                                let stream_position = {
-                                    start: start,
-                                    end: end
-                                }
-                                let stream = fs.createReadStream(file, stream_position);
-                                stream.on('open', function() {
-                                    stream.pipe(res);
-                                });
-                                stream.on('error', function(err) {
-                                    return next(err);
-                                });
-                            })
-                        }
-                    })
+                COSITEM.getObject({Bucket: bucket, Key: item}, function (error, data) {
+                    doStreaming(item, data.Body, res, req);
                 })
             }
         })
     }
 }
 module.exports = CloudObjectStorage;
+
+function doStreaming(item, cosFile, res, req) {
+    let stream = new Duplex();
+    stream.push(cosFile);
+    stream.push(null);
+    let buf = [];
+    let nb = 0;
+    let file = '.tmp/' + item;
+    stream.on('data', function(chunk) {
+        buf.push(chunk);
+        nb += chunk.length;
+    }).on('end', function() {
+        let buffer = Buffer.concat(buf, nb);
+        fs.writeFile(file, buffer, "binary", function(err, result) {
+            if (err) {
+                return err;
+            } else {
+                fs.stat(file, function(error, stats) {
+                    if (error) {
+                        //	1.	Check if the file exists
+                        if (error.code === 'ENOENT') {
+                            // 	->	404 Error if file not found
+                            return res.sendStatus(404);
+                        }
+                        //	2.	IN any other case, just output the full error
+                        return next(err)
+                    }
+                    let range = req.headers.range;
+                    if (!range) {
+                        let err = new Error('Wrong range');
+                        err.status = 416;
+                        //	->	Send the error and stop the request.
+                        //return next(err);
+                    }
+                    let positions = range.replace(/bytes=/, '').split('-');
+                    let start = parseInt(positions[0], 10);
+                    let file_size = stats.size;
+                    let end = positions[1] ? parseInt(positions[1], 10) : file_size - 1;
+                    let chunksize = (end - start) + 1;
+                    let head = {
+                        'Content-Range': 'bytes ' + start + '-' + end + '/' + file_size,
+                        'Accept-Ranges': 'bytes',
+                        'Content-Length': chunksize,
+                        'Content-Type': 'video/mp4'
+                    }
+                    res.writeHead(206, head);
+                    let stream_position = {
+                        start: start,
+                        end: end
+                    }
+                    let stream = fs.createReadStream(file, stream_position);
+
+                    stream.on('open', function() {
+                        stream.pipe(res);
+                    });
+                    stream.on('error', function(err) {
+                        return next(err);
+                    });
+                })
+            }
+        })
+    })
+}
